8c96b3a9cfc2607b320d274b060cbb097ef12f40
Kaique <kaiquegmoficial@gmail.com>
2026-03-05 22:08:51 -0300

    Ajustando BG

--- resources/views/livewire/components/about.blade.php
+++ resources/views/livewire/components/about.blade.php
@@ -1,69 +1,71 @@
-<section class="flex flex-col gap-10 max-w-screen-2xl mx-auto justify-center items-center pt-10 pb-30 px-2 md:px-16 font-sans bg-linear-to-b from-white via-white via-88% to-brand-blue_4" id="sobre">
-    <div class="flex flex-col justify-center items-center max-w-4xl text-center">
-        <span
-            class="bg-linear-to-b from-brand-blue_3 to-brand-blue bg-clip-text text-transparent font-bold md:text-[24px]">QUEM
-            SOMOS</span>
-        <h2 class="text-black text-3xl md:text-5xl font-bold">
-            Sobre a <strong class="bg-linear-to-b from-primary to-secondary bg-clip-text text-transparent"> Origem
-                3D </strong>
-        </h2>
-        <p class="text-brand-gray_2 md:leading-8 md:text-[24px] mt-2 md:mt-5">
-            Somos uma empresa especializada em impressão 3D, focada em criar produtos personalizados de alta qualidade
-            para pessoas e empresas que buscam inovação.
-        </p>
-    </div>
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-5">
-        <div class="relative">
-            <div
-                class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
-            </div>
-            <div
-                class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
-                <div
-                    class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
-                    <i class="fas fa-bullseye"></i>
-                </div>
-                <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Nossa Missão</h3>
-                <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
-                    Tornar a impressão 3D acessível a todos, transformando criatividade em objetos reais com
-                    qualidade, inovação e precisão.
-                </p>
-            </div>
+<section class="font-sans bg-linear-to-b from-white via-white via-88% to-brand-blue_4" id="sobre">
+    <div class="flex flex-col gap-10 max-w-screen-2xl mx-auto justify-center items-center pt-10 pb-30 px-2 md:px-16">
+        <div class="flex flex-col justify-center items-center max-w-4xl text-center">
+            <span
+                class="bg-linear-to-b from-brand-blue_3 to-brand-blue bg-clip-text text-transparent font-bold md:text-[24px]">QUEM
+                SOMOS</span>
+            <h2 class="text-black text-3xl md:text-5xl font-bold">
+                Sobre a <strong class="bg-linear-to-b from-primary to-secondary bg-clip-text text-transparent"> Origem
+                    3D </strong>
+            </h2>
+            <p class="text-brand-gray_2 md:leading-8 md:text-[24px] mt-2 md:mt-5">
+                Somos uma empresa especializada em impressão 3D, focada em criar produtos personalizados de alta qualidade
+                para pessoas e empresas que buscam inovação.
+            </p>
         </div>
-
-        <div class="relative">
-            <div
-                class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
-            </div>
-            <div
-                class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-5">
+            <div class="relative">
                 <div
-                    class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
-                    <i class="fas fa-star"></i>
+                    class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
                 </div>
-                <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Nosso Diferencial</h3>
-                <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
-                    Combinamos criatividade, tecnologia e atenção aos detalhes para entregar peças únicas que superam
-                    expectativas.
-                </p>
-            </div>
-        </div>
-
-        <div class="relative">
-            <div
-                class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
-            </div>
-            <div
-                class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
                 <div
-                    class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
-                    <i class="fas fa-microchip"></i>
+                    class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
+                    <div
+                        class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
+                        <i class="fas fa-bullseye"></i>
+                    </div>
+                    <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Nossa Missão</h3>
+                    <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
+                        Tornar a impressão 3D acessível a todos, transformando criatividade em objetos reais com
+                        qualidade, inovação e precisão.
+                    </p>
                 </div>
-                <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Tecnologia</h3>
-                <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
-                    Utilizamos impressoras de última geração e filamentos de alta performance, com materiais premium
-                    para garantir precisão, resistência e durabilidade.
-                </p>
+            </div>
+
+            <div class="relative">
+                <div
+                    class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
+                </div>
+                <div
+                    class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
+                    <div
+                        class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Nosso Diferencial</h3>
+                    <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
+                        Combinamos criatividade, tecnologia e atenção aos detalhes para entregar peças únicas que superam
+                        expectativas.
+                    </p>
+                </div>
+            </div>
+
+            <div class="relative">
+                <div
+                    class="absolute -left-1 top-0 h-full w-full bg-linear-to-b from-primary via-secondary to-brand-blue rounded-4xl">
+                </div>
+                <div
+                    class="relative bg-white shadow-[0_10px_10px_rgba(0,0,0,0.15)] z-10 h-full border-y-2 border-r-2 border-primary-border rounded-[30px] px-5 py-5 flex flex-col justify-center items-start">
+                    <div
+                        class="bg-linear-to-r from-primary via-secondary to-brand-blue rounded-xl text-white text-2xl md:text-3xl p-2 mb-1">
+                        <i class="fas fa-microchip"></i>
+                    </div>
+                    <h3 class="text-black font-bold text-2xl md:text-4xl mb-1 md:mb-3">Tecnologia</h3>
+                    <p class="text-brand-gray_2 text-lg md:text-xl leading-6">
+                        Utilizamos impressoras de última geração e filamentos de alta performance, com materiais premium
+                        para garantir precisão, resistência e durabilidade.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
