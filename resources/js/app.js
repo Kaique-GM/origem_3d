c2283048837cfe86afb87311445123284962fe14
Kaique-GM <kaiquegmoficial@gmail.com>
2026-03-06 12:52:20 -0300

    Ajustando espaço entre as imgs no swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -49,6 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         loop: true,
         speed: 800,
+        spaceBetween: 10,
         autoplay: {
             delay: 3000,
             disableOnInteraction: false
